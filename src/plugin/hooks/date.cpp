d1b8a26702e3b7d6367803ae977f39f6f68505e5
Cccc_ <me@iscccc.eu.org>
2026-02-15 15:47:42 +0800

    fix: date display

--- src/plugin/hooks/date.cpp
+++ src/plugin/hooks/date.cpp
@@ -10,9 +10,9 @@ namespace Date {
 		if (BytePattern::temp_instance().has_size(1, "DateFormat")) {
 			uintptr_t address = BytePattern::temp_instance().get_first().address();
 
-			// y 0x0F m w d 0x0E
+			// y 0x0F m w d 0x0E  →  ex) 1444年11月11日
 			char dateFormat[] = { 'y', ' ', 0x0F, ' ', 'm', 'w', ' ', 'd', ' ', 0x0E, 0 };
-			Injector::WriteMemoryRaw(address - 2, dateFormat, sizeof(dateFormat) - 1, true);
+			Injector::WriteMemoryRaw(address, dateFormat, sizeof(dateFormat), true);
 		}
 		else {
 			e.date.unmatchdDateProc1Injector = true;
